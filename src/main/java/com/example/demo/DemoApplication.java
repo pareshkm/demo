a5cea319abd2e3d25ca62cb93aa9ff1d06d0c332
Mohapatra, Paresh X <Paresh.X.Mohapatra@HealthPartners.Com>
2017-04-12 00:45:03 -0500

    More clean-up

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -29,30 +29,54 @@ public class DemoApplication {
 	public void loadProductData() {
 		List<Product> products = new ArrayList<Product>();
 		
-		Product prd1 = new Product();
-		prd1.setId(13860428);
-		CurrentPrice cp1 = new CurrentPrice();
-		cp1.setValue(new BigDecimal(100.53));
-		cp1.setCurrency_code(CurrencyCode.USD.name());
-		prd1.setCurrent_price(cp1);
-		products.add(prd1);
+		Product product1 = new Product();
+		product1.setId(13860428);
+		CurrentPrice currentPrice1 = new CurrentPrice();
+		currentPrice1.setValue(new BigDecimal(100.53));
+		currentPrice1.setCurrency_code(CurrencyCode.USD.name());
+		product1.setCurrent_price(currentPrice1);
+		products.add(product1);
 		
-		Product prd2 = new Product();
-		prd2.setId(1383242);
-		CurrentPrice cp2 = new CurrentPrice();
-		cp2.setValue(new BigDecimal(360.57));
-		cp2.setCurrency_code(CurrencyCode.DNR.name());
-		prd2.setCurrent_price(cp2);
-		products.add(prd2);
+		Product product2 = new Product();
+		product2.setId(15117729);
+		CurrentPrice currentPrice2 = new CurrentPrice();
+		currentPrice2.setValue(new BigDecimal(360.57));
+		currentPrice2.setCurrency_code(CurrencyCode.DNR.name());
+		product2.setCurrent_price(currentPrice2);
+		products.add(product2);
 		
-		Product prd3 = new Product();
-		prd3.setId(532727);
-		CurrentPrice cp3 = new CurrentPrice();
-		cp3.setValue(new BigDecimal(1476.68));
-		cp3.setCurrency_code(CurrencyCode.INR.name());
-		prd3.setCurrent_price(cp3);
-		products.add(prd3);
+		Product product3 = new Product();
+		product3.setId(16483589);
+		CurrentPrice currentPrice3 = new CurrentPrice();
+		currentPrice3.setValue(new BigDecimal(1476.68));
+		currentPrice3.setCurrency_code(CurrencyCode.INR.name());
+		product3.setCurrent_price(currentPrice3);
+		products.add(product3);
 		
+		Product product4 = new Product();
+		product4.setId(16696652);
+		CurrentPrice currentPrice4 = new CurrentPrice();
+		currentPrice4.setValue(new BigDecimal(960.57));
+		currentPrice4.setCurrency_code(CurrencyCode.USD.name());
+		product4.setCurrent_price(currentPrice4);
+		products.add(product4);
+		
+		Product product5 = new Product();
+		product5.setId(16752456);
+		CurrentPrice currentPrice5 = new CurrentPrice();
+		currentPrice5.setValue(new BigDecimal(1116.68));
+		currentPrice5.setCurrency_code(CurrencyCode.USD.name());
+		product5.setCurrent_price(currentPrice5);
+		products.add(product5);
+		
+		Product product6 = new Product();
+		product6.setId(15643793);
+		CurrentPrice currentPrice6 = new CurrentPrice();
+		currentPrice6.setValue(new BigDecimal(210.27));
+		currentPrice6.setCurrency_code(CurrencyCode.INR.name());
+		product6.setCurrent_price(currentPrice6);
+		products.add(product6);
+				
 		productService.saveProducts(products);
 	}
 }
--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,8 +1,5 @@
 package com.example.demo.entity;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
-import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -13,10 +10,6 @@ public class Product {
 	@Id
 	private long id;
 	
-	@AttributeOverrides ({
-		@AttributeOverride(name="value", column=@Column(name="value")),
-		@AttributeOverride(name="currency_code", column=@Column(name="currency_code"))
-	})
 	@Embedded
 	private CurrentPrice current_price;
 	
